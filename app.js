ebcbf9b1baa41435df8c720d9aeb3032956b8dd9
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-01 21:48:48 -0300

    add pointer cursor and open menu when searching feature

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 function toggleMenu() {
     const sidebarElements = document.querySelectorAll("#sidebar-menu, #sidebar-menu *");
+    sidebarElements.forEach(element => element.classList.toggle("closed"));
+}
 
-    sidebarElements.forEach(element => element.classList.toggle("closed"))
+function openMenu() {
+    const sidebarElements = document.querySelectorAll("#sidebar-menu, #sidebar-menu *");
+    sidebarElements.forEach(element => element.classList.remove("closed"));
+    
+    const searchInput = document.querySelector("#search-input");
+    searchInput.focus();
 }
